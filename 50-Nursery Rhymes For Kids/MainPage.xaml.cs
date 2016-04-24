38e369e214af48eefccb178d5913d7611c51c89f
salmanfarisvp <farissalmannbr@gmail.com>
2016-04-24 16:15:02 +0530

    Add project files.

---
+++ 50-Nursery Rhymes For Kids/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace _50_Nursery_Rhymes_For_Kids
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+       
+
+        private void abcd_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+        private void blacksheep_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void chuby_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void dingdong_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void earlytobed_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rusleeping_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+    }
+
